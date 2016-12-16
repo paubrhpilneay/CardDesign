6494e4dfd59d37322cf7863af9a3e6a15767bfd0
Abhinay Varma <abhinayvarma@Abhinays-MacBook-Air.local>
2016-12-16 17:21:23 +0530

    Changes With Firebase

---
+++ CardsDesign/GetFirebaseData.swift
@@ -0,0 +1,28 @@
+//
+//  GetFirebaseData.swift
+//  CardsDesign
+//
+//  Created by Abhinay Varma on 16/12/16.
+//  Copyright © 2016 Abhinay Varma. All rights reserved.
+//
+
+import UIKit
+import Firebase
+
+class GetFirebaseData: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func hitServer(_ sender: Any) {
+        
+        
+    }
+}
---
+++ CardsDesign/RestaurantModel.swift
@@ -0,0 +1,30 @@
+//
+//  RestaurantModel.swift
+//  CardsDesign
+//
+//  Created by Abhinay Varma on 16/12/16.
+//  Copyright © 2016 Abhinay Varma. All rights reserved.
+//
+
+import Foundation
+import Firebase
+
+class FIRDataObject: NSObject {
+    
+    let snapshot: FIRDataSnapshot
+    var key: String { return snapshot.key }
+    var ref: FIRDatabaseReference { return snapshot.ref }
+    
+    required init(snapshot: FIRDataSnapshot) {
+        
+        self.snapshot = snapshot
+        
+        super.init()
+        
+        for child in snapshot.children.allObjects as? [FIRDataSnapshot] ?? [] {
+            if responds(to: Selector(child.key)) {
+                setValue(child.value, forKey: child.key)
+            }
+        }
+    }
+}
